efe2259bd148f11d7b56a4ae3806eb365a5f74df
John Doe <jdoe@local>
2016-07-29 18:39:20 -0400

    Added additional mvc packages, and initial view for testing.

--- MiscLearn2-CustOrder-UI/Startup.cs
+++ MiscLearn2-CustOrder-UI/Startup.cs
@@ -15,17 +15,37 @@ namespace MiscLearn2_CustOrder_UI
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            //app.Map("/test", app2 => MyConfigure(app2));
+
+            app.UseIISPlatformHandler(options => options.AuthenticationDescriptions.Clear());
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public void MyConfigure(IApplicationBuilder app)
+        {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.UseMvc(routeBuilder =>
+            //{
+            //    routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            //});
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
 
         // Entry point for the application.
