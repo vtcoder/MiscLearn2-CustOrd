20edf0e92236576de5bd7e0f3f8ac438afe5e31e
John Doe <jdoe@local>
2016-08-01 17:33:51 -0400

    Added initial customer list page.

--- MiscLearn2-CustOrder-UI/Controllers/CustomerController.cs
+++ MiscLearn2-CustOrder-UI/Controllers/CustomerController.cs
@@ -3,19 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-using MiscLearn2_CustOrder_UI.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MiscLearn2_CustOrder_UI.Controllers
 {
-    public class HomeController : Controller
+    public class CustomerController : Controller
     {
         // GET: /<controller>/
         public IActionResult Index()
         {
-            var homeViewModel = new HomeViewModel() { HomeTitle = "Welcome to the MiscLearn2 Test App!" };
-            return View(homeViewModel);
+            return View();
         }
     }
 }
